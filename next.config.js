b054ff9a113e8cfa3ab9b52ec1a6352f7ea79835
Mary Ng <marypcng@hotmail.com>
2024-08-17 22:09:10 +1000

    Update build dir

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  distDir: 'build',
   compiler: {
     // ssr and displayName are configured by default
     styledComponents: true,
