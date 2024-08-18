ab9f016fa7ab1940c64ef273e61e1dbba0d5835a
Mary Ng <marypcng@hotmail.com>
2024-08-18 21:10:31 +1000

    export

---
+++ build/_next/static/pLtOanQEkvV2VSi6ecl8O/_buildManifest.js
@@ -0,0 +1,10 @@
+(self.__BUILD_MANIFEST = {
+  __rewrites: { afterFiles: [], beforeFiles: [], fallback: [] },
+  '/': [
+    'static/chunks/607-2565f351c53ab2dd.js',
+    'static/chunks/pages/index-03f3e6ce66f5adc9.js',
+  ],
+  '/_error': ['static/chunks/pages/_error-77823ddac6993d35.js'],
+  sortedPages: ['/', '/_app', '/_error'],
+}),
+  self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB();
